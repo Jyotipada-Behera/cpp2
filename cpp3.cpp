f04674ece5ae088c263d52e64d4dfba9c8e1c05f
Jyotipada <207305228+Jyotipada-Behera@users.noreply.github.com>
2026-03-21 11:10:38 +0530

    sum of n natural numbers

---
+++ cpp3.cpp
@@ -0,0 +1,10 @@
+#include "cpp3.h"
+
+int main() {
+	cpp3 c;
+	cout << "Enter a number: ";
+	cin >> c.n;
+	if (c.n <= 0) cout << "Please enter a positive integer." << endl;
+	else cout << "The sum of the first " << c.n << " natural numbers is: " << c.sum(c.n) << endl;
+	return 0;
+}
---
+++ cpp3.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class cpp3 {
+	public:
+		int n;
+		
+		int sum(int n) {
+			return n*(n + 1) / 2;
+		}
+};
+
