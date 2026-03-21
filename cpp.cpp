b8b63316c2447ccbd4dcc3f2bc260bbf06859dbe
Jyotipada <207305228+Jyotipada-Behera@users.noreply.github.com>
2026-03-21 10:37:31 +0530

    n days before d

--- cpp.cpp
+++ cpp.cpp
@@ -1,8 +1,23 @@
 #include "cpp.h"
 #include <iostream>
 
-int main()
-{
-	std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+int day(int d, int n) {
+	if (n == 0) return d;
+	if (d < 0) d+=7;
+	return day(d-1, n-1);
+}
+
+int main() {
+	int d, n;
+	cout << "Enter a day d = ";
+	cin >> d;
+
+	cout << "Enter n = ";
+	cin >> n;
+
+	cout << "The day n days before d is " << day(d, n) << endl;
+
 	return 0;
 }
