a9cfc59b01c56f4dfa0449191fa848e917068162
Jyotipada <207305228+Jyotipada-Behera@users.noreply.github.com>
2026-03-21 11:21:21 +0530

    nth term of GP

---
+++ cpp5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int nth_gp(int n, int a, int r) {
+	return a * pow(r, n - 1);
+}
+
+int main() {
+	int n, a, r;
+	cout << "Enter the term number (n), first term (a), and common ratio (r) of the geometric progression: ";
+	cout << "n: ";
+	cin >> n;
+	cin >> a;
+	cin >> r;
+	cout << "The " << n << "th term of the geometric progression is: " << nth_gp(n, a, r) << endl;
+}
